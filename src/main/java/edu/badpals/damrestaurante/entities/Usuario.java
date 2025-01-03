cfe1247e0fe2df914b58c25e7530b345f049ff8c
Victor <Victorsancheznogueira@gmail.com>
2025-01-03 12:09:18 +0100

    feat: usuario has Inheritance type joined

--- src/main/java/edu/badpals/damrestaurante/entities/Usuario.java
+++ src/main/java/edu/badpals/damrestaurante/entities/Usuario.java
@@ -7,6 +7,7 @@ import java.util.Collection;
 import java.util.Objects;
 
 @Entity
+@Inheritance(strategy= InheritanceType.JOINED)
 public class Usuario {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Id
